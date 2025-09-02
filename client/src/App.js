ec19a416e04576c433dd12e7e0daf72b9e04669a
Hyun <eric0909kim@gmail.com>
2025-09-01 21:31:02 -0400

    add routing for signup, login, upload, and logs pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,24 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import SignupPage from './pages/SignupPage';
+import LoginPage from './pages/LoginPage';
+import LogsPage from './pages/LogsPage'
+import UploadPage from './pages/UploadPage';
 
 function App() {
   return (
     <Router>
+      <nav>
+        <Link to='/'>Homepage</Link>
+        <Link to='/uploadCSV'>Upload</Link>
+        <Link to='/logs'>Logs</Link>
+        <Link to='/login'>Login</Link>
+        <Link to='/signup'>Signup</Link>
+      </nav>
+
       <Routes>
-        <Route path='/' element={<Homepage />} />
-        <Route path="/upload" element={<UploadPage />} />
+        <Route path='/' />
+        <Route path="/uploadCSV" element={<UploadPage />} />
         <Route path="/logs" element={<LogsPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignupPage />} />
